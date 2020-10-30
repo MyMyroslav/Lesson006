4cb0ff514ad048d099a873bdc9ac39cbe417a4e8
Myroslav <havrushkomyroslav@gmail.com>
2020-10-30 15:44:29 +0200

    Don-Lesson006

---
+++ Lesson006_Task1/Lesson006_Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lesson006_Task1
+{
+    class MainClass
+    {
+        static void CalculateIncrement(ref int a)
+        {
+            int res = ++a;
+            Console.WriteLine(res);
+        }
+        static void CalculateDecrement(ref int a)
+        {
+            int res = a--;
+            Console.WriteLine(res);
+        }
+        public static void Main(string[] args)
+        {
+            int a = 12;
+            CalculateDecrement(ref a);
+            CalculateIncrement(ref a);
+            
+        }
+    }
+}
---
+++ Lesson006_Task1/Lesson006_Task2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Lesson006_Task2
+{
+    class MainClass
+    {
+        static void iAnd ( int a,  int b)
+        {
+            int res = a & b;
+            Console.WriteLine($"And={res}");
+            
+        }
+        static void iOr( int a,  int b)
+        {
+            int res = a | b;
+            Console.WriteLine($"Or={res}");
+            
+        }
+        static void iEOR( int a,  int b)
+        {
+            int res = a ^ b;
+            Console.WriteLine($"EOr={res}");
+            
+        }
+        static void iNotA(int a)
+        {
+            int res = ~a;
+            Console.WriteLine($"Not A ={res}");
+        }
+        static void iNotb(int b)
+        {
+            int res = ~b;
+            Console.WriteLine($"Not B ={res}");
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A:");
+            string a = Console.ReadLine();
+            Console.WriteLine("Enter B:");
+            string b = Console.ReadLine();
+            int iA = int.Parse(a);
+            int iB = int.Parse(b);
+            Console.WriteLine("Enter Operation (&,|,^,~A,~B):");
+            string operation = Console.ReadLine();
+            if (operation == "&")
+            {
+                iAnd( iA,  iB);
+            }
+            else if(operation == "|")
+            {
+                iOr( iA,  iB);
+            }
+            else if (operation == "^")
+            {
+                iEOR( iA,  iB);
+            }
+            else if (operation == "~A")
+            {
+                iNotA(iA);
+            }
+            else if (operation == "~B")
+            {
+                iNotb(iB);
+            }
+            else
+            {
+                Console.WriteLine("Wrong operation");
+            }
+            
+        }
+    }
+}
